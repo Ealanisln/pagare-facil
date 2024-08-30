e5d4af1c810803d64b9bc3940311b44641b97a18
Emmanuel Alanis <ealanisln@me.com>
2024-08-29 19:29:26 -0700

    :bug: Corrected import

--- src/components/PromissoryNotePDF/index.tsx
+++ src/components/PromissoryNotePDF/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
-import { formatearCantidad } from '../../lib/number-to-letter';
+import { formatearCantidad } from '@/lib/number-to-letter';
 
 type Guarantor = {
   name: string;
